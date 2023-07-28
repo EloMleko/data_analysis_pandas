bce07c204c2ac3f8e7e1087a3b6637e771a419c4
EloMleko <mateusz.strelczuk2@wp.pl>
2023-07-28 16:15:50 +0200

    next 10 ex

--- dataframe.py
+++ dataframe.py
@@ -91,7 +91,7 @@ df19 = df2.drop(['attempts'], axis=1)
 
 df2['color'] = [random.choice(['Red', 'Blue', 'Green'])
                 for _ in range(df2.shape[0])]
-
+df2.pop('color')
 print(df2)
 
 # 21
@@ -101,3 +101,70 @@ df21 = pd.DataFrame([{'name': 'Anastasia', 'score': 12.5}, {
 
 for _, row in df21.iterrows():
     print(row['name'], row['score'])
+
+# 22
+
+df22 = list(df2.columns)
+
+# 23
+
+df23 = pd.DataFrame([[1, 4, 7], [2, 5, 8], [3, 6, 9]],
+                    columns=['col1', 'col2', 'col3'])
+df23.columns = ['Column1', 'Column2', 'Column3']
+
+# 24
+
+df24 = pd.DataFrame([[1, 4, 7], [4, 5, 8], [3, 6, 9], [4, 7, 0], [
+                    5, 8, 1]], columns=['col1', 'col2', 'col3'])
+df24_2 = df24[df24['col1'] == 4]
+df24_1 = df24.loc[df24['col1'] == 4]
+
+# 25
+
+df25 = df24.copy()
+df25 = df25.reindex(columns=['col3', 'col2', 'col1'])
+
+# 26
+
+df26 = df24.copy()
+df26.loc['5'] = [10, 11, 12]
+
+# 27
+
+df27 = df24.copy()
+df27.to_csv('Tab.csv', sep='\t')
+df27_1 = pd.read_csv('Tab.csv')
+
+# 28
+
+df28 = pd.DataFrame({'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+                     'city': ['California', 'Los Angeles', 'California', 'California',
+                              'California', 'Los Angeles', 'Los Angeles', 'Georgia', 'Georgia', 'Los Angeles']})
+
+df28_1 = df28.groupby(['city']).count().reset_index()
+df28_2 = df28.groupby(['city']).size().reset_index(name='number of people')
+
+# 29
+
+df29 = df24.copy()
+df29 = df29.loc[df29['col3'] != 8]
+df29 = df29.drop(df29.loc[df29['col3'] == 8].index)
+
+# 30
+
+# 31
+
+df31 = df24.copy()
+idx = 2
+df31 = df31.iloc[[idx]]
+
+# 32 
+
+df32 = df2.copy()
+df32 = df32.fillna(0)
+
+# 33
+
+df33 = df2.copy()
+df33.reset_index(drop=True, inplace=True)
+print(df33)
